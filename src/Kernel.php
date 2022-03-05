67437faa5a65d82dc3c8b1d576ab95e15d0bf548
Alexey <pitrider@mail.ru>
2022-03-05 21:39:36 +0300

    Fix: remaining abstract methods

--- src/Kernel.php
+++ src/Kernel.php
@@ -3,9 +3,33 @@
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Routing\RouteCollectionBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+    public function registerBundles()
+    {
+        return [
+            new \Symfony\Bundle\FrameworkBundle\FrameworkBundle()
+        ];
+    }
+
+    protected function configureContainer(ContainerBuilder $c, LoaderInterface $loader)
+    {
+        // PHP equivalent of config/packages/framework.yaml
+        $c->loadFromExtension('framework', [
+            'secret' => 'S0ME_SECRET'
+        ]);
+    }
+
+    protected function configureRoutes(RouteCollectionBuilder $routes)
+    {
+        // kernel is a service that points to this class
+        // optional 3rd argument is the route name
+        //$routes->add('/random/{limit}', 'kernel::randomNumber');
+    }
 }
